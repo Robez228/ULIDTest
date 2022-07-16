ae1d3c5db124f938c36faf6b1356ab78490ddcfb
Яковенко Ярослав <robezgames@mail.ru>
2022-07-17 02:29:01 +0300

    Добавьте файлы проекта.

---
+++ NulidTest.cs
@@ -0,0 +1,39 @@
+﻿using NUlid;
+using NUlid.Rng;
+
+namespace ULIDTest;
+
+internal static class NulidTest
+{
+
+
+
+    public static void PrintMonotonicUlid(int n = 1)
+    {
+        if (n < 1)
+        {
+            return;
+        }
+
+        var rng = new MonotonicUlidRng();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(NUlid.Ulid.NewUlid(rng));
+        }
+    }
+
+    public static void PrintUlid(int n = 1)
+    {
+        if (n < 1)
+        {
+            return;
+        }
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(NUlid.Ulid.NewUlid());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace ULIDTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new();
+        
+        stopwatch.Start();
+        NulidTest.PrintUlid(100);
+        stopwatch.Stop();
+        Console.WriteLine("-----");
+        Console.WriteLine(stopwatch.ElapsedMilliseconds.ToString());
+        Console.WriteLine("-----");
+
+        stopwatch.Restart();
+        NulidTest.PrintMonotonicUlid(100);
+        stopwatch.Stop();
+        Console.WriteLine("-----");
+        Console.WriteLine(stopwatch.ElapsedMilliseconds.ToString());
+        Console.WriteLine("-----");
+    }
+}
